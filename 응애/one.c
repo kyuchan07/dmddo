1de01199dabb3c6bffaf90b144ba7c36cde04473
kyuchan07 <103513869+kyuchan07@users.noreply.github.com>
2022-04-16 13:37:35 +0900

    초기 커밋.

---
+++ 응애/one.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void main()
+{
+	//printf("Hello world\n 암 스타팅더 씨발럼아 언어\n");
+	//printf("응애");
+	//printf("55 + 13=%d\n", 55 + 13);
+	//printf("100 - 40=%d", 100 - 40); 
+	int a, b;
+	scanf("%d", &a);
+	printf("+\n");
+	scanf("%d", &b);
+	printf("=\n");
+	printf("%d\n", a + b);
+}
